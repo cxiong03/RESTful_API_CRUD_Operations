4b8fb60b8f0e07d26ab506890f5c4ebba660438d
Chang Xiong <cnxiong03@gmail.com>
2019-12-16 20:16:47 -0500

    video 5 complete

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to MongoDb...'))
     .catch(err => console.log('Could not connect to MongoDb...', err))
+
+    const courseSchema = new mongoose.Schema({
+        name: String,
+        author: String,
+        tags: [ String ],
+        date: { type: Date, default: Date.now },
+        isPublished: Boolean
+    });
