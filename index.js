2e007ff819d85a8198994b5f49a506554feb83c5
Chang Xiong <cnxiong03@gmail.com>
2019-12-19 11:51:44 -0500

    video 17 complete

--- index.js
+++ index.js
@@ -36,7 +36,17 @@ async function getCourses() {
         .skip((pageNumber - 1) * pageSize)
         .limit(pageSize)
         .sort({ name: 1 })
-        .count() // counts the number of filter
+        .select({ name: 1, tags: 1 });
     console.log(courses);
 }
-getCourses();
+
+async function updateCourse(id) {
+    const course = await Course.findById(id);
+    if (!course) return;
+    course.isPublished = true;
+    course.author = 'Another Author';
+    
+    const result = await course.save();
+    console.log(result);
+}
+updateCourse('5df8343b76a500322d2d6698');
